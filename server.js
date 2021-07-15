0c9cadf97efd2ecb305f6ee8db6b1d92361a1e06
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-07-15 23:06:35 +0530

    testsssssss

--- server.js
+++ server.js
@@ -12,6 +12,39 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
+//Server Middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+
+//Importing Routes
+const authRoute = require("./routes/authroute")
+
+
+//Configuring Dotenv
+dotenv.config();
+//DB Connection
+mongoose.connect(process.env.DATABASE_ACCESS, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+}, () => { console.log("Database Connected SuccessFully..!!") })
+
+
+//Calling Of All Routes
+app.use("/app", authRoute);
+
+
+//Production Route
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
 
 
 //Server Listening At This Port
