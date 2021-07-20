6da0bbe37aba5d0371d410296729889248e1f502
SHivam21082000 <shivamshukla752@gmail.com>
2021-07-20 23:33:08 +0530

    navbar done
    completed the navbar

---
+++ client/src/components/Quora.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './navbar'
+function Quora() {
+    return (
+        <div className='quora'>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Quora
