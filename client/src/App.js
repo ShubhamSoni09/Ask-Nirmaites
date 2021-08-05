71c9af2ded2c52a0042dea3f1ef613ccd8f43423
Shubham Soni <46859348+ShubhamSoni09@users.noreply.github.com>
2021-08-06 01:25:42 +0530

    redux part

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,17 @@
 import './css/App.css';
 
 import Quora from './components/Quora'
+import Login from './components/auth/login';
+import { selectUser} from './features/userSlice';
+import { useSelector } from "react-redux";
+
 function App() {
+  const user = useSelector(selectUser)
   return (
     <div className="App">
+      {
+      user ? (<Quora/>) : (<Login/>)
+      }
       <header className="App-header">
         <Quora />
       </header>
